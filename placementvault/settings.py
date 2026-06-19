703d370268f337ea2556b6bb44d67f5e363b4280
Vivek Bisht <vivekbisht9868335112@gmail.com>
2026-06-19 16:45:47 +0530

    deployment setup

--- placementvault/settings.py
+++ placementvault/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-seyv44bk4a2q$l324^82#71u#2)k_mki7ebvhtqcu%^-c0ti%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
